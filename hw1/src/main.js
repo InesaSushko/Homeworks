f185e7a2451c3190aaedf4ddcb215f8d71d6e336
Inesa <inesatat@ukr.net>
2017-05-17 20:35:33 +0300

    'homework'

---
+++ hw1/src/main.js
@@ -0,0 +1,19 @@
+const person1 = {
+	firstName: 'Anna',
+	lastName: 'Petrova',
+	number: 0800000000,
+	homeNumber: 0545454544,
+	email: 'person1@gmail.com',
+	groups: ['Family', 'Best Friends'],
+	socMedia: {
+		twitter: 'Anna_Petrova',
+		facebook: 'Anna Petrova'
+	},
+	guickCall: true,
+    extraNumber: {
+    	number2: 0800888888,
+    	number2Owner: 'Tatiana',
+    	number2Relation: 'Mother'
+    }
+};
+
